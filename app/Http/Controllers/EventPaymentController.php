e8066ba51b2274190187f5bea9a222810a8177a7
jason-heroes620 <jason.w@heroes.my>
2024-04-17 13:58:35 +0800

    changed redirect class

--- app/Http/Controllers/EventPaymentController.php
+++ app/Http/Controllers/EventPaymentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
 use App\Models\EventPayments;
 use App\Models\PaymentCategories;
