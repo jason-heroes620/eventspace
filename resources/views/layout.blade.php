83910bd4a107ce16ccb679e9117761b4325e2de1
jason-heroes620 <jason.w@heroes.my>
2024-05-21 12:38:14 +0800

    added csrf toke in header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
     <link href="{{ asset('/css/loading.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/izitoast@1.4.0/dist/css/iziToast.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <!-- <meta name="csrf-token" content="{{ csrf_token() }}" /> -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 
 <body>
